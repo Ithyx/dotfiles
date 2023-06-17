cf50d827fed5aa9d25cb9582fdd17194a59cd50e
Ithyx <lamidey.m@gmail.com>
2023-06-17 23:06:48 +0900

    Added prototype nu parser

--- nvim/init.lua
+++ nvim/init.lua
@@ -295,6 +295,17 @@ require 'nvim-treesitter.configs'.setup {
     },
 }
 
+--- Add nu parser
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.nu = {
+  install_info = {
+    url = "https://github.com/nushell/tree-sitter-nu",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+  filetype = "nu",
+}
+
 -- setup some extensions
 vim.filetype.add {
     extension = {
