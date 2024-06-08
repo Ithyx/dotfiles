f8622f1f18fc60649fcb697281b7f310a01fb46f
Ithyx <lamidey.m@gmail.com>
2024-06-08 17:29:43 -0400

    Updated diagnostic sign definition for new versions

--- nvim/init.lua
+++ nvim/init.lua
@@ -70,14 +70,37 @@ vim.api.nvim_command('set autoread')
 vim.api.nvim_command('set termguicolors')
 
 -- custom icons
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+            [vim.diagnostic.severity.HINT] = "󰌵",
+        },
+        texthl = {
+            [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+            [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+            [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+            [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+            [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+            [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+            [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+        },
+    },
+})
+
 vim.fn.sign_define("DiagnosticSignError",
-    { text = " ", texthl = "DiagnosticSignError" })
+    { text = " ", texthl = "DiagnosticSignError", numhl = "DiagnosticSignError" })
 vim.fn.sign_define("DiagnosticSignWarn",
-    { text = " ", texthl = "DiagnosticSignWarn" })
+    { text = " ", texthl = "DiagnosticSignWarn", numhl = "DiagnosticSignWarn" })
 vim.fn.sign_define("DiagnosticSignInfo",
-    { text = " ", texthl = "DiagnosticSignInfo" })
+    { text = " ", texthl = "DiagnosticSignInfo", numhl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint",
-    { text = "󰌵", texthl = "DiagnosticSignHint" })
+    { text = "󰌵", texthl = "DiagnosticSignHint", numhl = "DiagnosticSignHint" })
 
 
 -- basic mapping
