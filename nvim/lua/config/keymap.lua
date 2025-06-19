6f4d3d4ac0af60adf6104f4456c6f8d8c9431ce6
Ithyx <lamidey.m@gmail.com>
2025-06-18 22:37:36 -0400

    Add keybind for displaying full diagnostics

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -3,3 +3,5 @@ vim.keymap.set('n', '<A-Left>', '<C-o>')
 vim.keymap.set('n', '<A-Right>', '<C-I>')
 vim.keymap.set('n', '<C-Down>', 'gj')
 vim.keymap.set('n', '<C-Up>', 'gk')
+
+vim.keymap.set('n', '<C-d>', vim.diagnostic.open_float)
