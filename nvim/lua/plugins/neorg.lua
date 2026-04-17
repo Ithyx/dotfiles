cfb9a5dcc76d820e6db38e93e6a98ee0c89ec37e
Ithyx <mathis.lamidey@ithyx.dev>
2026-04-16 22:27:27 -0700

    Add neorg plugin

---
+++ nvim/lua/plugins/neorg.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "nvim-neorg/neorg",
+        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+        version = "*", -- Pin Neorg to the latest stable release
+        config = true,
+        dependencies = {
+            'nvim-neorg/tree-sitter-norg',
+            'nvim-neorg/tree-sitter-norg-meta',
+        },
+
+        opts = {
+            load = {
+                ['core.defaults'] = {},
+                ['core.concealer'] = {},
+                ['core.summary'] = {},
+                ['core.dirman'] = {
+                    config = {
+                        workspaces = {
+                            main = '~/notes/main/', -- Format: <name_of_workspace> = <path_to_workspace_root>
+                            EA = '~/notes/EA/',
+                        },
+                        index = 'index.norg',
+                        default_workspace = 'main',
+                    },
+                },
+            },
+        },
+    }
+}
